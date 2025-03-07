d8511330c11da172bd3821436283089771191111
Roberto Cerna <164534193+robertoCerna1055@users.noreply.github.com>
2025-03-07 10:48:55 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -83,45 +83,77 @@ class CustomButton extends StatefulWidget {
 
 class _CustomButtonState extends State<CustomButton> {
   bool _isHovered = false;
+  bool _isPressed = false;
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      cursor: SystemMouseCursors.click, // Cambia el cursor a "pointer" (mano)
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 200),
-        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        decoration: BoxDecoration(
-          color: _isHovered ? widget.color.withOpacity(0.8) : widget.color,
-          borderRadius: BorderRadius.circular(0),
-        ),
-        child: Row(
-          children: [
-            if (widget.icon != null) Icon(widget.icon, color: Colors.white),
-            if (widget.icon != null) SizedBox(width: 6),
-            Text(
-              widget.label,
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-            SizedBox(width: 6),
-            Container(
-              padding: EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
+    return GestureDetector(
+      onTapDown: (_) => setState(() => _isPressed = true),
+      onTapUp: (_) => setState(() => _isPressed = false),
+      onTapCancel: () => setState(() => _isPressed = false),
+      onTap: () {
+        // Lógica al hacer clic
+        print("Botón ${widget.label} presionado");
+      },
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        cursor: SystemMouseCursors.click, // Cambia el cursor a "pointer" (mano)
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 200),
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            color: _isPressed
+                ? widget.color.withOpacity(0.6) // Efecto al presionar
+                : _isHovered
+                    ? widget.color.withOpacity(0.8) // Efecto al pasar el cursor
+                    : widget.color,
+            borderRadius: BorderRadius.circular(8), // Bordes redondeados
+            boxShadow: _isPressed
+                ? [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
+                    ),
+                  ]
+                : _isHovered
+                    ? [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ]
+                    : [],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (widget.icon != null) Icon(widget.icon, color: Colors.white),
+              if (widget.icon != null) SizedBox(width: 6),
+              Text(
+                widget.label,
+                style: TextStyle(color: Colors.white, fontSize: 16),
               ),
-              child: Text(
-                "6",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+              SizedBox(width: 6),
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Text(
+                  "6",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
